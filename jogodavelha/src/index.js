8b939a012681a4b3a3b989626ec6fa0e1002b2b8
DiegoFernandesdeSa <diegofernandesdesa@gmail.com>
2021-07-19 20:28:44 -0300

    Criando um componente interativo!

--- jogodavelha/src/index.js
+++ jogodavelha/src/index.js
@@ -4,14 +4,23 @@ import './index.css';
 
 
 class Square extends React.Component {
-    render(){
-        return(
-            <button className="square" onClick={()=>alert('Um clique feito com uma Arrow Function, que bacana!')}>
-                {this.props.value}
-            </button>
-        );
-    }
+constructor(props) {        
+    super(props);
+    this.state={
+            value: null,
+        };
+    }    
+    render() {
+            return(
+        <button className="square" 
+       onClick={()=>this.setState({value: 'X'})}>
+       {this.state.value}
+                            </button>);
 }
+}
+
+
+    
 class Board extends React.Component {
     renderSquare(i){
         return<Square value={i} />;
